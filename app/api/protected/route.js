d4e1b5502b42425fdbf9d77564fbcee504621aa0
Bahaeeddine27 <kiliminelbooking@gmail.com>
2025-01-06 16:29:47 +0100

    fifth commit

--- app/api/protected/route.js
+++ app/api/protected/route.js
@@ -6,7 +6,15 @@ export async function GET(request) {
   const authHeader = request.headers.get('Authorization');
 
   // 2. Extraire le token du "Authorization" header
-  const token = authHeader?.split(' ')[1]; // Le token se trouve après "Bearer"
+  if (!authHeader) {
+    // Si l'en-tête est absent, renvoyer une erreur 401
+    return NextResponse.json(
+      { message: 'Token is required' },
+      { status: 401 }
+    );
+  }
+
+  const token = authHeader.split(' ')[1]; // Le token se trouve après "Bearer"
 
   // 3. Vérifier la présence du token
   if (!token) {
@@ -17,13 +25,13 @@ export async function GET(request) {
   }
 
   try {
-    // 4. Vérifier le JWT avec la clé secrète pour l'access token
+    // 3. Vérifier le JWT avec la clé secrète pour l'access token
     const decoded = jwt.verify(token, process.env.JWT_SECRET); // Utilisation de la clé secrète pour l'access token
 
-    // 5. Renvoyer la réponse avec les données décodées
+    // 4. Renvoyer la réponse avec les données décodées
     return NextResponse.json({ decoded });
   } catch (error) {
-    // 6. Renvoyer une erreur si le token est invalide ou expiré
+    // 5. Renvoyer une erreur si le token est invalide ou expiré
     return NextResponse.json(
       { message: 'Invalid or expired token' },
       { status: 403 }
